81c94862e0c12debd41ac999fa7205f6eed81725
Pushkar-Gameti <parveengameti2@gmail.com>
2025-12-13 20:33:42 +0530

    add script.js

---
+++ first/calculator.js
@@ -0,0 +1,9 @@
+export let sumData=(a,b)=>{
+    return a + b;
+}
+
+export let mulData=(a,b)=>{
+    return a * b;
+}
+
+export let cName = "Pushkar";
---
+++ first/index.js
@@ -0,0 +1,5 @@
+import {sumData as sum, mulData as mul, cName as name} from "./calculator.js";
+
+console.log(sum(5,10));
+console.log(mul(5,10));
+console.log(name);
