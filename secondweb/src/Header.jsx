1f8bac2f7b1a4d100640c9f9ffa5e00e0ab12d38
Pushkar-Gameti <parveengameti2@gmail.com>
2025-12-14 18:59:25 +0530

    add second and third web

--- secondweb/src/Header.jsx
+++ secondweb/src/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Header.css'
+import googleLogo from './images/google_logo.svg';
 
 export default function Header() {
   return (
-    <div className='header' > Header Code</div>
+    <div className='header' > 
+    <img src={googleLogo} alt='googlelogo'/>
+    <h4>Header </h4>
+    </div>
   )
 }
