84ca86b307d5e2a5d20897c87de0c8a53ff34cef
Pushkar-Gameti <parveengameti2@gmail.com>
2025-12-16 20:29:06 +0530

    done

--- thirdweb/src/App.js
+++ thirdweb/src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
 import { useState } from 'react';
+import btnModule from './Button.module.css';
 
 function App() {
   let [status, setStatus] = useState(false);
+  let [pstatus, setPstatus] = useState(false);
+  let [menuStatus, setMenuStatus] = useState(false);
+  let [modalStatus, setModalStatus] = useState(false);
+
   return (
     <div className="App">
+
+      <button className='en' onClick={()=>setModalStatus(!modalStatus)}>Enquire Now</button>
+      <div className={`modalOverLay ${modalStatus?'modelShow':''}`}></div>
+      <div className={`ModalDiv ${modalStatus?'showModalDiv':''}`}>
+        <h3>My Website</h3>
+      </div>
+
+
+      <button className='micon' onClick={()=>setMenuStatus(!menuStatus)}>
+        {menuStatus ? <span>&times;</span> : <span>&#9776;</span>}
+      </button>
+      <div className={`menu ${menuStatus?'activeMenu':''}`}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Services</li>
+        </ul>
+      </div>
+      <input type={pstatus ? 'text' : 'password'}/> 
+      <button onClick={()=>setPstatus(!pstatus)}>
+        {pstatus ? 'Hide' : 'Show'}</button>
+<br></br>
+
+      <button className={btnModule.error}>error</button>
+      <button className={btnModule.warning}>warning</button>
       <button onClick={() => setStatus(!status)}>
         {status ? 'Hide' : 'Show'}
       </button>
